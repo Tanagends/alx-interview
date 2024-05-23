dbf4fa2daa207aa203c7afff9bc8953079989361
Tanagends <tanagends@gmail.com>
2024-05-23 23:18:59 +0200

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,14 +7,14 @@ def validUTF8(data):
     count = 0
     for num in data:
         if count == 0:
-            if num >> 7:
-                return False
-            elif num >> 5 == 110:
+            if num >> 5 == 110:
                 count = 2
             elif num >> 4 == 1110:
                 count = 3
             elif num >> 3 == 11110:
                 count = 4
+            elif count >> 7:
+                return False
         else:
             if num >> 6 != 0b10:
                 return False

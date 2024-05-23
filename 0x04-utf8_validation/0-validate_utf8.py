8f2114634123ac5af9dc0ba8770ee3382584fa15
Tanagends <tanagends@gmail.com>
2024-05-23 23:12:43 +0200

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,20 +3,20 @@
 
 
 def validUTF8(data):
-	"""validates utf-8"""
+    """validates utf-8"""
     count = 0
     for num in data:
         if count == 0:
-            if (num >> 5) == 0b110:
-                count = 1
-            elif (num >> 4) == 0b1110:
-                count = 2
-            elif (num >> 3) == 0b11110:
-                count = 3
-            elif (num >> 7):
+            if num >> 7:
                 return False
+            elif num >> 5 == 110:
+                count = 2
+            elif num >> 4 == 1110:
+                count = 3
+            elif num >> 3 == 11110:
+                count = 4
         else:
-            if (num >> 6) != 0b10:
+            if num >> 6 != 0b10:
                 return False
             count -= 1
     return count == 0
